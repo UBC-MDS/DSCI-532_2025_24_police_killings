d17fe412f9aeedc85d0f6badc7eb57771184175b
Adrian Leung <adrianleung726@gmail.com>
2025-03-05 14:22:00 -0800

    added styling in checkboxes

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -1,6 +1,13 @@
 import dash_bootstrap_components as dbc
 from dash import html, dcc
 
+year_options = [2015, 2016, '2015 & 2016']
+race_options = ['White', 'Black', 'Hispanic/Latino', 
+                'Asian/Pacific Islander', 'Native American', 
+                'Arab-American', 'Other']
+age_options = ['Under 19', '20-39', '40-59', 'Above 60', 'Unknown']
+armed_options = ['Unarmed', 'Firearm', 'Non-lethal firearm', 'Knife', 'Vehicle', 'Disputed', 'Other']
+
 sidebar = dbc.Col([
     html.H3('Global Controls'),
     html.H5('Year'),
@@ -12,28 +19,29 @@ sidebar = dbc.Col([
     html.Br(),
 
     html.H5('Race/Ethnicity'),
-    dcc.Checklist(["All"], ["All"], id="all-checklist-1", inline=True),
+    dcc.Checklist(options=[{'label': html.Span('All', style={'margin-left': '10px'}), 'value': 'All'}], 
+                  value=["All"], id="all-checklist-1", inline=True),
     dcc.Checklist(
         id='race-checklist', 
-        options=['White', 'Black', 'Hispanic/Latino', 
-                 'Asian/Pacific Islander', 'Native American', 
-                 'Arab-American', 'Other'],
+        options=[{'label': html.Span(race, style={'margin-left': '10px'}), 'value': race} for race in race_options]
         ), 
     
     html.Br(),
     html.H5('Age Group'),
-    dcc.Checklist(["All"], ["All"], id="all-checklist-2", inline=True),
+    dcc.Checklist(options=[{'label': html.Span('All', style={'margin-left': '10px'}), 'value': 'All'}], 
+                  value=["All"], id="all-checklist-2", inline=True),
     dcc.Checklist(
         id='age-checklist', 
-        options=['Under 19', '20-39', '40-59', 'Above 60', 'Unknown'],
+        options=[{'label': html.Span(age, style={'margin-left': '10px'}), 'value': age} for age in age_options],
         ),
 
     html.Br(),
     html.H5('Was the Victim Armed?'),
-    dcc.Checklist(["All"], ["All"], id="all-checklist-3", inline=True),
+    dcc.Checklist(options=[{'label': html.Span('All', style={'margin-left': '10px'}), 'value': 'All'}], 
+                  value=["All"], id="all-checklist-3", inline=True),
     dcc.Checklist(
         id='armed-checklist', 
-        options=['Unarmed', 'Firearm', 'Non-lethal firearm', 'Knife', 'Vehicle', 'Disputed', 'Other']
+        options=[{'label': html.Span(armed, style={'margin-left': '10px'}), 'value': armed} for armed in armed_options]
         ),
     ],
     md=2, className="custom-sidebar")
