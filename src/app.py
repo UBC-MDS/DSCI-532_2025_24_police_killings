c32138a81b8f69db1b42e077e6ce8aef5471e236
OliviaWan56 <55778380+OliviaWan56@users.noreply.github.com>
2025-03-05 22:37:36 -0800

    Break code v2 (#51)
    * update pages
    
    * update utils
    
    * update data
    
    * update app.py
    
    * create a Procfile
    
    * update app.py
    
    * update app.py
    
    * cleanup code files
    
    ---------
    
    Co-authored-by: RONG WAN <rwan03@student.ubc.ca>
    Co-authored-by: Adrian Leung <adrianleung726@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,43 +1,17 @@
-from dash import Dash, html
+import dash
 import dash_bootstrap_components as dbc
-from components.title import title
-from components.sidebar import sidebar
-from components.footer import footer
-from components.charts import two_charts
-from components.tab import tab
-from components.input import input_state
-import callbacks
+from pages.home import layout
+from utils.settings import DEBUG_MODE 
 
-# Initiatlize the app
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='Police Killings')
+# Initialize the app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='Police Killings')
 
-# Add this line for deployment compatibility
-server = app.server
+# Deployment server setup
+server = app.server  
 
-# Layout
-app.layout = dbc.Container([
-    dbc.Row([title], className='custom-header'),
-    dbc.Row([
-        sidebar,
-        dbc.Col([
-            tab,
-            html.Br(),
-            html.Br(),
-            dbc.Row([
-                dbc.Col([
-                    dbc.Row(
-                        dbc.Col([input_state], md=10)
-                    ), 
-                    html.Br(),
-                    two_charts
-                ]),
-            ]),
-        ], md=10),
-    ]),
-    html.Hr(),
-    dbc.Row(dbc.Col(footer))
-], fluid=True)
+# Set the app layout from home.py
+app.layout = layout
 
-# Run the app/dashboard
-if __name__ == '__main__':
-    app.run(debug=False)
+# Run the app
+if __name__ == "__main__":
+    app.run_server(debug=DEBUG_MODE)
---
+++ src/utils/settings.py
@@ -0,0 +1,2 @@
+DEBUG_MODE = False  # Set to False for production
+PORT = 8050
