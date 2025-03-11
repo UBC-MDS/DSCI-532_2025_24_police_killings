b6c64907f8788412f434ccfd67557cee7edf80ff
Adrian Leung <adrianleung726@gmail.com>
2025-03-11 00:52:40 -0700

    stored data in binary format and minor fix in removing decimal point in age

--- src/data/police_data.py
+++ src/data/police_data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def load_clean_data():
     """Load and preprocess the dataset."""
-    return pd.read_csv('data/processed/clean_data.csv')
+    return pd.read_parquet('data/processed/clean_data.parquet')
 
 police_data = load_clean_data()
